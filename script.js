b926a4b79a21335fdbbeaa993c14fa398973e1a1
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-12-05 02:09:11 +0300

    finished

--- script.js
+++ script.js
@@ -5,6 +5,34 @@ $(document).ready(function() {
         } else {
             $('.navbar').removeClass('sticky');
         }
+
+        if(this.scrollY > 100) {
+            $('.scroll-up-btn').addClass('show');
+        } else {
+            $('.scroll-up-btn').removeClass('show');
+        }
+    });
+
+    // scroll up btn script
+
+    $('.scroll-up-btn').click(function() {
+        $('html').animate({scrollTop: 0});
+    });
+
+    // typing animation script
+    
+    var typed = new Typed('.typing', {
+        strings: ['Ютубер', 'Разработчик', 'Блогер', 'Дизайнер', 'Фрилансер'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed('.typing-2', {
+        strings: ['Ютубер', 'Разработчик', 'Блогер', 'Дизайнер', 'Фрилансер'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // toogle menu/navbar script
@@ -13,6 +41,12 @@ $(document).ready(function() {
         $('.navbar .menu').toggleClass('active');
         $('.menu-btn i').toggleClass('active');
         $('body').toggleClass('modal');
+    });
+
+    $('.menu-link').click(function() {
+        $('.navbar .menu').removeClass('active');
+        $('.menu-btn i').removeClass('active');
+        $('body').removeClass('modal');
     });
 
     // owl carousel script
